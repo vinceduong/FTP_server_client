850e55c6668a1146f45d8c7fad5286ec6023fb4b
Vincent Duong <vincent.duong.dev@gmail.com>
2020-11-22 20:49:22 +0100

    wip

--- myFtpClient.js
+++ myFtpClient.js
@@ -11,11 +11,7 @@ const args = process.argv.slice(2);
 
 const [host = '127.0.0.1', port = 5000] = args;
 
-
-client.connect(port, host, async () => {
-
-  handleUserInput()
-})
+client.connect(port, host)
 
 client.on('data', data => {
   const dataString = data.toString()
@@ -25,11 +21,9 @@ client.on('data', data => {
 
   if (firstLine === 'FILE_CONTENT') {
     const fileName = dataString.split('\n')[1]
- 
     const fileContent = dataLines.slice(2).join('\n')
-    
     const filePath = `./ftp_${fileName}`
- 
+
     console.log(`client> Writing file to ${filePath}...`)
 
     fs.writeFile(filePath, fileContent, error => {
@@ -48,7 +42,6 @@ client.on('data', data => {
 
 function handleUserInput() {
   readline.question('>', input => {
-    console.log('input', input)
     if (input.split(' ')[0] === 'STOR') {
       const fileName = input.split(' ')[1];
 
@@ -70,12 +63,14 @@ client.on('error',
   error => {
     const { syscall } = error
     console.error(`client> Action ${syscall} failed... `)
-    console.error(error)
+
+    console.error('ftp>', error)
     process.exit(5)
   }
 )
 
 client.on('close', () => {
-  console.log('client> Server connection closed, quitting...')
+  console.log('client> Connection closed, quitting...')
+
   process.exit(0)
 })
--- myFtpServer.js
+++ myFtpServer.js
@@ -2,142 +2,148 @@ const net = require('net')
 const fs = require('fs')
 const path = require('path')
 
-const users = [
-  {
-    name: 'Jean',
-    password: 'mdp1'
-  },
-  {
-    name: 'Francois',
-    password: 'mdp2'
-  }
-]
+const users = require('./users.json')
 
 const [port = 5000] = process.argv.slice(2);
 
-const server = net.createServer((socket) => {
-  console.log('New client connected')
+const HELP =
+`
+LIST OF AVAILABLE COMMANDS:
+\tUSER <username>: check if the user exist
+\tPASS <password>: authenticate the user with a password
+\tLIST: list the current directory of the server
+\tCWD <directory>: change the current directory of the server
+\tRETR <filename>: transfer a copy of the file FILE from the server to the client
+\tSTOR <filename>: transfer a copy of the file FILE from the client to the server
+\tPWD: display the name of the current directory of the server
+\tHELP: send helpful information to the client
+\tQUIT: close the connection and stop the program
+`
 
+const server = net.createServer((socket) => {
+  console.log('New client connected...')
+
+  // Connection variables
   let sessionUser = null;
   let authentificated = true;
   let currentPath = path.resolve('./');
 
-  console.log('currentPath', currentPath) 
   socket.write('Connected to FTP server')
+
   socket.on('data', (data) => {
-  try {
-    const message = data // Buffer < 0x00 ... >
-    .toString() // PASS passsword\r\n
-    .replace('\r\n', '') // PASS password
-    .split(' ') // ['PASS', 'password']
+    try {
+      const [directive, parameter] = data.toString().split(' ')
 
-    const directive = message[0]
-    const parameter = message[1]
+      switch (directive) {
+        case 'USER':
+          const user = users.find(user => user.name === parameter)
 
-    switch (directive) {
-      case 'USER':
-        const user = users.find(user => user.name === parameter)
-
-        if (user) {
-          sessionUser = user
-          socket.write('User OK')
-        } else {
-          socket.write('User not found')
-        }
-        break
-      case 'PASS':
-        if (authentificated) {
-          socket.write('Already authentificated')
-        } else if (sessionUser === null) {
-          socket.write('Enter a valid user using the USER command')
-        } else if (sessionUser.password !== parameter) {
-          socket.write('Wrong password please retry')
-        } else {
-          socket.write('Authentificated')
-          authentificated = true
-        }
-        break
-
-      case 'LIST':
-        if (authentificated) {
-          const files = fs.readdirSync(currentPath)
-
-          socket.write('LIST \n' + files.join(' '))
-        } else {
-          socket.write('Forbidden: Not authentificated')
-        }
-        break
-      case 'CWD':
-        if (authentificated) {
-          try {
-            const newPath = path.resolve(currentPath + '/' + parameter)
-
-            fs.readdirSync(newPath)
-            
-            socket.write(`CWD ${parameter}`)
-            currentPath = newPath
-          } catch(error) {
-            socket.write(`${parameter} does not exists or isn't a directory`)
+          if (user) {
+            sessionUser = user
+            socket.write('User OK')
+          } else {
+            socket.write('User not found')
           }
-        } else {
-          socket.write('Forbidden: Not authentificated')
-        }
-        break
-      case 'PWD':
-        if (authentificated) {
-          socket.write('PWD ' + currentPath)
-        } else {
-          socket.write('Forbidden: Not authentificated')
-        }
-        break
-      case 'RETR':
-        if (authentificated) {
-          fs.readFile(`${currentPath}/${parameter}`, (err, data) => {
-            if (err) {
-              socket.write(`Error while opening file, file ${currentPath}/${parameter} does not exists`)
-            } else {
-              socket.write('FILE_CONTENT\n' + `${parameter}\n` + data)
+          break
+        case 'PASS':
+          if (authentificated) {
+            socket.write('Already authentificated')
+          } else if (sessionUser === null) {
+            socket.write('Enter a valid user using the USER command')
+          } else if (sessionUser.password !== parameter) {
+            socket.write('Wrong password please retry')
+          } else {
+            socket.write('Authentificated')
+            authentificated = true
+          }
+          break
+
+        case 'LIST':
+          if (authentificated) {
+            const files = fs.readdirSync(currentPath)
+
+            socket.write('LIST \n' + files.join(' '))
+          } else {
+            socket.write('Forbidden: Not authentificated')
+          }
+          break
+        case 'CWD':
+          if (authentificated) {
+            try {
+              const newPath = path.resolve(currentPath + '/' + parameter)
+
+              /*
+              Checking if new path exists if fs.readdirSync throws an error,
+              it means the directory doesn't exists
+              */
+              fs.readdirSync(newPath)
+
+              socket.write(`CWD ${parameter}`)
+              currentPath = newPath
+            } catch (error) {
+              socket.write(`${parameter} does not exists or isn't a directory`)
             }
-          })
-        } else {
-          socket.write('Forbidden: Not authentificated')
-        }
-        break
-      case 'STOR':
-        if (authentificated) {
-          const input = data.toString().split('\n')
+          } else {
+            socket.write('Forbidden: Not authentificated')
+          }
+          break
+        case 'PWD':
+          if (authentificated) {
+            socket.write('Current directory is: ' + currentPath)
+          } else {
+            socket.write('Forbidden: Not authentificated')
+          }
+          break
+        case 'RETR':
+          if (authentificated) {
+            fs.readFile(`${currentPath}/${parameter}`, (err, data) => {
+              if (err) {
+                socket.write(`Error while opening file, file ${currentPath}/${parameter} does not exists`)
+              } else {
+                socket.write('FILE_CONTENT\n' + `${parameter}\n` + data)
+              }
+            })
+          } else {
+            socket.write('Forbidden: Not authentificated')
+          }
+          break
+        case 'STOR':
+          if (authentificated) {
+            const inputLines = data.toString().split('\n')
 
- 
-          const fileData = input.slice(1).join('\n')
+            const fileData = inputLines.slice(1).join('\n') + '\n'
 
-          console.log('fileData', fileData)
+            console.log('fileData', fileData)
 
-          const filePath = `client_${input[0][1]}`
- 
-          fs.writeFile(filePath, fileData, error => {
-            if (error) {
-              console.error(error)
-            } else {
-              socket.write(`Client> File saved at ${filePath}`)
-            }
-          })
-        } else {
-          socket.write('FILE_CONTENT\n' + `${parameter}\n` + data)
-        }
-        break
-      case 'QUIT':
-        socket.write('Quitting ftp server... closing connection')
-        socket.destroy()
-        break
-      default:
-        socket.write(`404 Command "${directive}" not found`)
+            const filePath = `client_${inputLines[0][1]}`
+
+            fs.writeFile(filePath, fileData, error => {
+              if (error) {
+                console.error(error)
+              } else {
+                socket.write(`Client> File saved at ${filePath}`)
+              }
+            })
+          } else {
+            socket.write('FILE_CONTENT\n' + `${parameter}\n` + data)
+          }
+          break
+        case 'QUIT':
+          socket.destroy()
+          break
+        case 'HELP':
+          socket.write(HELP)
+          break
+        default:
+          socket.write(`Command "${directive}" invalid, type HELP for more info`)
+      }
+    } catch (error) {
+      console.error(error)
+
+      socket.write('Internal server error... closing connection...')
+      socket.destroy(error)
     }
-  } catch (error) {
-    console.error(error)
-    socket.write('Internal server error... closing connection...')
-    socket.destroy(error)
-  }
-    
+
   })
 })
 
